a7cdd32a07cf5c249ab2d0a701de9399ab1c2721
jiandaoshou-aidehua <2893666867@qq.com>
2026-01-20 14:57:50 +0800

    fix air build failure on linux

--- setup.sh
+++ setup.sh
@@ -47,7 +47,7 @@ else #linux
     # https://github.com/microsoft/AirSim/issues/4866
     wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
     sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-jammy.list http://packages.lunarg.com/vulkan/lunarg-vulkan-jammy.list
-    sudo apt update
+    # sudo apt update
     sudo apt-get -y install --no-install-recommends \
         lsb-release \
         rsync \
