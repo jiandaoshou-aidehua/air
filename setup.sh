e1ff361891a9e767628c220beb758cca1c3a47ce
jiandaoshou-aidehua <2893666867@qq.com>
2026-01-20 19:44:41 +0800

    修改setup.sh中clang版本

--- setup.sh
+++ setup.sh
@@ -64,7 +64,7 @@ else #linux
         wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
         sudo apt-get update
     fi
-    sudo apt-get install -y clang-10 clang++-10 libc++-dev libc++abi-dev
+    sudo apt-get install -y clang-11 clang++-11 libc++-dev libc++abi-dev
 fi
 
 if ! which cmake; then
